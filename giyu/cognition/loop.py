39a34fe5ecb71a66724a9cd93256fe81f382bc6d
blackeagle686 <mathematecs1@gmail.com>
2026-05-06 02:21:35 +0300

    Refactor GiyuLoop constructor to accept direct component parameters

--- giyu/cognition/loop.py
+++ giyu/cognition/loop.py
@@ -23,12 +23,13 @@ class GiyuLoop(AgentLoop):
     MAX_RETRIES = 2
     MAX_ACTIONS = 30
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if hasattr(self, "components") and "generator" in self.components:
-            self.generator = self.components["generator"]
-        else:
-            self.generator = GiyuGenerator(self.planner.llm)
+    def __init__(self, thinker, planner, actor, reflector, analyzer, 
+                 stability_scorer=None, decision_gate=None, correlation_engine=None, generator=None):
+        super().__init__(thinker, planner, actor, reflector, analyzer)
+        self.stability_scorer = stability_scorer
+        self.decision_gate = decision_gate
+        self.correlation_engine = correlation_engine
+        self.generator = generator or GiyuGenerator(self.planner.llm)
 
     @property
     def _ctx(self):
@@ -36,9 +37,9 @@ class GiyuLoop(AgentLoop):
         return dict(
             thinker=self.thinker, planner=self.planner, generator=self.generator,
             reflector=self.reflector, actor=self.actor, analyzer=self.analyzer,
-            stability_scorer=self.components.get("stability_scorer"),
-            decision_gate=self.components.get("decision_gate"),
-            correlation_engine=self.components.get("correlation_engine"),
+            stability_scorer=self.stability_scorer,
+            decision_gate=self.decision_gate,
+            correlation_engine=self.correlation_engine,
             llm=self.planner.llm, bg=self._background_tasks, retries=self.MAX_RETRIES,
         )
 
